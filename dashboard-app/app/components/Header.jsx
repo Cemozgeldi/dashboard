eb74792313a1787b0c24d7d30eb6b15f1f4682f6
cemozgeldi <cem.ozgeldi22@ogr.atauni.edu.tr>
2023-07-13 00:56:34 +0300

    create a account dashboard page and middleware.js for auth

--- dashboard-app/app/components/Header.jsx
+++ dashboard-app/app/components/Header.jsx
@@ -5,9 +5,29 @@ import React from "react";
 const Header = () => {
   return (
     <>
+      {/* we create this component with a fragment */}
       <nav className="bg-blue-700 py-4 px-6 flex items-center justify-between mb-5">
+        {/* we create a nav with tailwind classes and we use flexbox to align the
+        items */}
         <div className="flex items-center">
-          <div className="text-lg uppercase font-bold text-white">DashApp</div>
+          {/* we create a div with flexbox to align the items */}
+          <Link href="/">
+            {" "}
+            {/* we create a link to the home page */}
+            <div className="text-lg uppercase font-bold text-white">
+              DashApp
+            </div>
+          </Link>
+        </div>
+        <div className="text-white">
+          {" "}
+          {/* we create a sign in and sign up links in this div */}
+          <Link href="/sign-in" className="text-gray-300 hover:text-white mr-4">
+            Sign In
+          </Link>
+          <Link href="/sign-up" className="text-gray-300 hover:text-white mr-4">
+            Sign Up
+          </Link>
         </div>
       </nav>
     </>
